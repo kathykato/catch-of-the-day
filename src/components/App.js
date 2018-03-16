9f9306281c91060879a0a93e9b5dce3b8e8e1493
Katherine Kato <katherine.a.kato@gmail.com>
2018-03-15 19:37:39 -0700

    Added option to remove items

--- src/components/App.js
+++ src/components/App.js
@@ -46,7 +46,13 @@ class App extends React.Component {
     const fishes = { ...this.state.fishes };
     fishes[key] = updatedFish;
     this.setState({fishes});
-  }
+  };
+
+  deleteFish = (key) => {
+    const fishes = { ...this.state.fishes };
+    fishes[key] = null;
+    this.setState({ fishes });
+  };
 
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
@@ -55,6 +61,12 @@ class App extends React.Component {
   addToOrder = (key) => {
     const order = {...this.state.order};
     order[key] = order[key] + 1 || 1;
+    this.setState({ order });
+  };
+
+  removeFromOrder = (key) => {
+    const order = { ...this.state.order };
+    delete order[key];
     this.setState({ order });
   };
 
@@ -69,8 +81,8 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
-        <Inventory addFish={this.addFish} updateFish={this.updateFish} loadSampleFishes={this.loadSampleFishes} fishes={this.state.fishes} />
+        <Order fishes={this.state.fishes} order={this.state.order} removeFromOrder={this.removeFromOrder} />
+        <Inventory addFish={this.addFish} updateFish={this.updateFish} deleteFish={this.deleteFish} loadSampleFishes={this.loadSampleFishes} fishes={this.state.fishes} />
       </div>
     );
   }
